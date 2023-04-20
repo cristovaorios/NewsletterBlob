8d560464308c0da4bfa4c2eb5c044caccb9f0958
Pedro Lucas <79203933+pedrocslucas@users.noreply.github.com>
2023-04-20 19:52:39 -0300

    Blob News v01

---
+++ NewsletterBlob/Model/LeitorDAO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms.VisualStyles;
+
+namespace NewsletterBlob.Model
+{
+    internal class LeitorDAO
+    {
+    }
+}
