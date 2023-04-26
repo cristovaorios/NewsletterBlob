cd449f2bd16342000996d1b8fcf4cea9396edf37
Pedro Lucas <79203933+pedrocslucas@users.noreply.github.com>
2023-04-26 11:47:53 -0300

    Blob News v04

---
+++ NewsletterBlob/View/JanelaOpcoesAutor.Designer.cs
@@ -0,0 +1,142 @@
+﻿
+namespace NewsletterBlob.View
+{
+    partial class JanelaOpcoesAutor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(JanelaOpcoesAutor));
+            this.pctBoxPerfil = new System.Windows.Forms.PictureBox();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.btnMinhaConta = new System.Windows.Forms.Button();
+            this.btnCriarPublicacao = new System.Windows.Forms.Button();
+            this.btnEditarPublicacao = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pctBoxPerfil)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pctBoxPerfil
+            // 
+            this.pctBoxPerfil.BackColor = System.Drawing.Color.Transparent;
+            this.pctBoxPerfil.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pctBoxPerfil.Image = global::NewsletterBlob.Properties.Resources.Component_4;
+            this.pctBoxPerfil.Location = new System.Drawing.Point(228, 41);
+            this.pctBoxPerfil.Name = "pctBoxPerfil";
+            this.pctBoxPerfil.Size = new System.Drawing.Size(90, 90);
+            this.pctBoxPerfil.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pctBoxPerfil.TabIndex = 3;
+            this.pctBoxPerfil.TabStop = false;
+            // 
+            // btnSair
+            // 
+            this.btnSair.BackColor = System.Drawing.Color.Transparent;
+            this.btnSair.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSair.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSair.Font = new System.Drawing.Font("Poppins", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSair.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(75)))), ((int)(((byte)(128)))));
+            this.btnSair.Location = new System.Drawing.Point(91, 400);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(354, 49);
+            this.btnSair.TabIndex = 13;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = false;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // btnMinhaConta
+            // 
+            this.btnMinhaConta.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(75)))), ((int)(((byte)(128)))));
+            this.btnMinhaConta.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnMinhaConta.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMinhaConta.Font = new System.Drawing.Font("Poppins", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMinhaConta.ForeColor = System.Drawing.Color.White;
+            this.btnMinhaConta.Location = new System.Drawing.Point(91, 155);
+            this.btnMinhaConta.Name = "btnMinhaConta";
+            this.btnMinhaConta.Size = new System.Drawing.Size(354, 49);
+            this.btnMinhaConta.TabIndex = 12;
+            this.btnMinhaConta.Text = "Minha conta";
+            this.btnMinhaConta.UseVisualStyleBackColor = false;
+            this.btnMinhaConta.Click += new System.EventHandler(this.btnMinhaConta_Click);
+            // 
+            // btnCriarPublicacao
+            // 
+            this.btnCriarPublicacao.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(75)))), ((int)(((byte)(128)))));
+            this.btnCriarPublicacao.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCriarPublicacao.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCriarPublicacao.Font = new System.Drawing.Font("Poppins", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCriarPublicacao.ForeColor = System.Drawing.Color.White;
+            this.btnCriarPublicacao.Location = new System.Drawing.Point(91, 229);
+            this.btnCriarPublicacao.Name = "btnCriarPublicacao";
+            this.btnCriarPublicacao.Size = new System.Drawing.Size(354, 49);
+            this.btnCriarPublicacao.TabIndex = 14;
+            this.btnCriarPublicacao.Text = "Criar publicação";
+            this.btnCriarPublicacao.UseVisualStyleBackColor = false;
+            this.btnCriarPublicacao.Click += new System.EventHandler(this.btnCriarPublicacao_Click);
+            // 
+            // btnEditarPublicacao
+            // 
+            this.btnEditarPublicacao.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(75)))), ((int)(((byte)(128)))));
+            this.btnEditarPublicacao.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnEditarPublicacao.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEditarPublicacao.Font = new System.Drawing.Font("Poppins", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEditarPublicacao.ForeColor = System.Drawing.Color.White;
+            this.btnEditarPublicacao.Location = new System.Drawing.Point(91, 301);
+            this.btnEditarPublicacao.Name = "btnEditarPublicacao";
+            this.btnEditarPublicacao.Size = new System.Drawing.Size(354, 49);
+            this.btnEditarPublicacao.TabIndex = 15;
+            this.btnEditarPublicacao.Text = "Editar publicação";
+            this.btnEditarPublicacao.UseVisualStyleBackColor = false;
+            this.btnEditarPublicacao.Click += new System.EventHandler(this.btnEditarPublicacao_Click);
+            // 
+            // JanelaOpcoesAutor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::NewsletterBlob.Properties.Resources.Login1;
+            this.ClientSize = new System.Drawing.Size(536, 490);
+            this.Controls.Add(this.btnEditarPublicacao);
+            this.Controls.Add(this.btnCriarPublicacao);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnMinhaConta);
+            this.Controls.Add(this.pctBoxPerfil);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "JanelaOpcoesAutor";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Blob News";
+            ((System.ComponentModel.ISupportInitialize)(this.pctBoxPerfil)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pctBoxPerfil;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Button btnMinhaConta;
+        private System.Windows.Forms.Button btnCriarPublicacao;
+        private System.Windows.Forms.Button btnEditarPublicacao;
+    }
+}
